35b39328abeac36d72ec589c9bc5da2d83d2c410
sevrugin <sevrugin@gmail.com>
2016-01-18 08:14:33 +0200

    Библиотека выключателя

--- firmware/lib-switch.lua
+++ firmware/lib-switch.lua
@@ -1,17 +1,45 @@
 return {
   config = {
-    title = '',
-    lib = 'switch',
-    pin = nil,
+    title = '', -- тектовое описание
+    lib = 'switch', -- используемая библиотека
+    pin = nil, -- пин управления
+    min = 0,
+    max = 1023,
     delay = 0, -- Плавность включения
     onchange = false -- Броадкаст статуса при смене значения
   },
-  init= function(config)
+  init = function(config)
+    local module = {
+        status = gpio.LOW,
+        config = config
+    }
+    function module:get()
+        return self.status
+    end
+    function module:set(value)
+        if value == 'ON' then
+          value = self.config.max
+        elseif value == 'OFF' then
+          value = self.config.min
+        end
+        -- onchange
+        if self.status ~= value then
+          self.status()
+        end
+        self.status = value
+        if self.config.pin ~= nil then
+          gpio.write(self.config.pin, value)
+        end
+    end
+    function module:status()
+      --broadcast self.status
+    end
+
+    if module.config.pin ~= nil then
+      gpio.mode(config.pin, gpio.OUTPUT)
+      module.set(gpio.LOW)
+    end
+    
+    return module
   end,
-  get = function()
-  end,
-  set = function (value)
-  end,
-  status = function()
-  end
 }
